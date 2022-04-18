e9a8b5a5c2780e3e8c585bcc3ea0d596ad63f062
Giorgos Dougkas <georgios.dougkas@eurodyn.com>
2022-04-18 20:18:07 +0300

    init commit

---
+++ src/main/java/com/example/team2/Team2Application.java
@@ -0,0 +1,13 @@
+package com.example.team2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team2Application.class, args);
+	}
+
+}
